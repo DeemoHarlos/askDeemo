8e62e6293d6698ea71a8b64836cf053fbce4c1e6
DeemoHarlos <cces4415@gmail.com>
2017-11-19 08:05:54 +0800

    just commit for server

---
+++ admin/index.js
@@ -0,0 +1,111 @@
+/*General Function Declaration*/
+function $(a,b){
+	var e = document.querySelectorAll(a)
+	if (e.length == 1) return e[0]
+	else return e
+}
+function $n(a,id,cls){
+	var e = document.createElement(a)
+	if(id) e.id = id
+	for(cl of Array.prototype.slice.call(arguments,2))
+		e.classList.add(cl)
+	return e
+}
+function $a(e,a){
+	e.insertAdjacentHTML('beforeend',a)
+	return e
+}
+function $g(e,n,v){
+	if(v) e.setAttribute(n,v)
+	else return e.getAttribute(n)
+}
+
+function clicked(e,f){
+	e.addEventListener('click',f)
+}
+function parseDate(d){
+	var date = new Date(d)
+	var str = ''
+	str += date.getFullYear() + '/'
+	str += (''+(date.getMonth()+1)).padStart(2,'0') + '/'
+	str += (''+date.getDate()).padStart(2,'0') + '<br>'
+	/*var utc = -date.getTimezoneOffset()/60
+	str += '(' + ((utc>0)?('+'+utc):(utc==0?'0':utc)) + ')\n'*/
+	str += (''+date.getHours()).padStart(2,'0') + ':'
+	str += (''+date.getMinutes()).padStart(2,'0') + ':'
+	str += (''+date.getSeconds()).padStart(2,'0') + '.'
+	str += (''+date.getMilliseconds()).padStart(3,'0') + ' '
+	return str
+}
+function parseIp(ip){
+	var ipv4Regex = /^(\d{1,3}\.){3,3}\d{1,3}$/
+	var ipv6Regex = /^(::)?(((\d{1,3}\.){3}(\d{1,3}){1})?([0-9a-f]){0,4}:{0,2}){1,8}(::)?$/i
+	var nip
+	if(ip.slice(0,7)=='::ffff:')nip = ip.slice(7)
+	nip = nip.split(".").map(x=>('<span>'+x+'</span>')).join(".")
+	return nip
+}
+
+var data
+var label = ['ip','time','key','content','answer','ansTime','userAgent','ansButton']
+var hidden = [false,false,false,false,false,false,false,false]
+var input = $n('textarea',null,'ansBox')
+$g(input,'name','answer')
+$g(input,'cols','40')
+$g(input,'rows','5')
+var update = $n('input',null,'updateButton')
+$g(update,'type','button')
+$g(update,'value','Update')
+
+
+function toggle(i,h){
+	$('.'+label[i]).forEach((e,i,a)=>{e.classList.toggle('hide')})
+	if(h) $('#h-'+label[i]).classList.toggle('hide')
+}
+
+function update(){
+	var req = new XMLHttpRequest()
+	var server = 'http://deemo.pw:11520'
+	req.open('GET',server)
+	req.setRequestHeader('Content-Type','application/X-www-form-urlencoded')
+	req.send()
+	req.onreadystatechange = function () {
+		if(req.readyState === XMLHttpRequest.DONE){
+			console.log(server + ' responded with status ' + req.status)
+			if(req.status>=200 && req.status<400){
+				data = eval(req.response).reverse()
+				$('#table .content').remove()
+				$('#table>table').append($n('tbody','','content'))
+				var t = $('#table .content')
+				var uid = data.length
+				var l = Math.log(10,uid)
+				data.forEach((e,i,a)=>{
+					var tr = $n('tr')
+					tr.append($a($n('td',null,'uid'),(''+uid--).padStart(l,'0')))
+					tr.append($a($n('td',null,'ip'),parseIp(e.ip)))
+					tr.append($a($n('td',null,'time'),parseDate(e.time)))
+					tr.append($a($n('td',null,'key'),e.key))
+					tr.append($a($n('td',null,'content'),e.content))
+					var inputBox = input.cloneNode(true);
+					inputBox.value = e.ans||''
+					tr.append($n('td',null,'answer').append(inputBox))
+					tr.append($a($n('td',null,'ansTime'),e.ansTime))
+					tr.append($a($n('td',null,'userAgent'),e.userAgent))
+					var updateButton = update.cloneNode(true);
+					$g(updateButton,'id','e.id')
+					$g(updateButton,'onclick','updateAns()')
+					tr.append($n('td',null,'answer').append(updateButton))
+					t.append(tr)
+				})
+				for(i in label)if(hidden[i]) toggle(i,false)
+			}
+		}
+	}
+}
+$('#check>*').forEach((e,i,a)=>{
+	clicked(e,()=>{
+		hidden[i] = !hidden[i]
+		toggle(i,true)
+		e.style.backgroundColor = hidden[i]?'transparent':'#555'
+	})
+})
--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const log = require('./logger')
+const bcrypt = require('bcrypt')
 const port = argv.p || 80
 const database = (argv.d || '127.0.0.1:27017')
+const mykey = '$2a$10$HzFpcQmmIOd9iDiDohCuqOfIEWYFHb9BKZgZsDlovACjy7BaEdfWi'
 
 log.printLog('info','Starting server ...')
 
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
-var visitorSchema = new Schema({
+var questionSchema = new Schema({
 	ip: {type: String},
-	port: {type: Number},
 	time: {type: Date},
-	protocol: {type: String},
-	reqHost: {type: String},
-	resource: {type: String},
-	website: {type: String},
+	content: {type: String},
+	key: {type: String},
+	ans: {type: String},
+	ansTime: {type: Date},
 	userAgent: {type: String}
 })
-var Visitor = mongoose.model('Visitor', visitorSchema)
+var Question = mongoose.model('Visitor', questionSchema)
 
 log.printLog('info','Connecting to database ...')
 mongoose.connect(database, function (err, res) {
@@ -43,30 +44,55 @@ app.use((req,res,next)=>{
 })
 
 app.post('/',(req,res)=>{
-	var newVisitor = new Visitor()
-	newVisitor.ip = req.ip || 'unknown'
-	newVisitor.port = req.connection.remotePort || 'unknown'
-	newVisitor.time = new Date(Date.now())
-	newVisitor.protocol = req.protocol || 'unknown'
-	newVisitor.reqHost = req.get('host') || 'unknown'
-	newVisitor.resource = req.originalUrl || 'unknown'
-	newVisitor.website = req.body.website || 'unknown'
-	newVisitor.userAgent = req.body.userAgent || 'unknown'
-	log.printLog('info',newVisitor)
-	newVisitor.save((err, course)=>{
+	var newQuestion = new Question()
+	if(!req.body.ip) return res.status(403).send('IP not found.')
+	if(!req.body.content || !req.body.content.length) return res.status(403).send('No content.')
+	newQuestion.ip = req.body.ip
+	newQuestion.time = new Date(Date.now())
+	newQuestion.content = req.body.content
+	newQuestion.userAgent = req.body.userAgent || 'unknown'
+	newQuestion.key = req.body.key || ''
+	if(bcrypt.compareSync(req.body.key,mykey))
+		return res.status(403).send('Unavailable key.')
+	log.printLog('info',newQuestion)
+	newQuestion.save((err, question)=>{
 		if (err) {
 			log.printLog('error',err)
 			res.status(403).send(err)
 		}
-		else res.status(200).send(newVisitor);
+		else res.status(200).send(newQuestion);
+	})
+})
+
+app.post('/ans',(req,res)=>{
+	if (!req.body.key || !bcrypt.compareSync(req.body.key,mykey))
+		return res.status(403).send('Forbidden.')
+	var newQuestion = new Question()
+	if(!req.body.id) return res.status(403).send('No question assigned.')
+	if(!req.body.content || !req.body.content.length) return res.status(403).send('No content.')
+	newQuestion.ans = req.body.content
+	newQuestion.ansTime = new Date(Date.now())
+	log.printLog('info',newQuestion)
+	Question.findOneAndUpdate(req.body.id, newQuestion, function (err, question) {
+		if (err) return res.status(403).send(err)
+		res.json(question)
 	})
 })
 
 app.get('/',(req,res)=>{
-	Visitor.find({}, function (err, visitor) {
-		if (err) { return res.status(403).send(err) }
-		res.status(200).json(visitor)
-	})
+	if (bcrypt.compareSync(req.body.key,mykey)){	
+		Question.find({}, function (err, question) {
+			if (err) return res.status(403).send(err)
+			res.status(200).json(question)
+		})
+	}
+	else if (req.body.key){
+		Question.find({key:req.body.key}, function (err, question) {
+			if (err) return res.status(403).send(err)
+			res.status(200).json(question)
+		})
+	}
+	else return res.status(404).send('Not found.')
 })
 
 app.listen(port, ()=>{
